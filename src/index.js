5878fccf51b6bb68050231b7576e6ff9a1a57a6c
CenenAG <agtecnologia.mex@gmail.com>
2022-08-18 13:21:08 -0700

    Nuevo Componente Saludar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { TaskCard } from "./Task";
+import {Saludar} from './Saludar'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
     <TaskCard ready={true} />
+    <Saludar />
   </>
 );
